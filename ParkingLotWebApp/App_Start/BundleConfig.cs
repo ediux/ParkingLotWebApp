2d1224cc01c73e4216edd53f84abe70c686777b9
ediux <ediux@msn.com>
2016-10-27 07:42:53 +0800

    [變更] 復原bootstrap的虛擬路徑

--- ParkingLotWebApp/App_Start/BundleConfig.cs
+++ ParkingLotWebApp/App_Start/BundleConfig.cs
@@ -20,11 +20,10 @@ namespace ParkingLotWebApp
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap.min.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/bs/css").Include(
-                      "~/Content/bootstrap.css",
+            bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.min.css",
                       "~/Content/site.css"));
         }
