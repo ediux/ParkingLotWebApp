d4c3bba95e90fe1cf638991ab22ca63e6ee18a16
ediux <ediuxcity@gmail.com>
2016-11-02 11:17:29 +0800

    [變更] 加入Create 靜態方法以初始化執行個體。

--- ParkingLotWebApp/Models/CarsInParkingLotPath_Header.Partial.cs
+++ ParkingLotWebApp/Models/CarsInParkingLotPath_Header.Partial.cs
@@ -3,12 +3,18 @@ namespace ParkingLotWebApp.Models
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-    
+
     [MetadataType(typeof(CarsInParkingLotPath_HeaderMetaData))]
     public partial class CarsInParkingLotPath_Header
     {
+        public static CarsInParkingLotPath_Header Create(int UserId)
+        {
+            var model = new CarsInParkingLotPath_Header();
+            model.CreateUTCTime = DateTime.Now.ToUniversalTime();
+            return model;
+        }
     }
-    
+
     public partial class CarsInParkingLotPath_HeaderMetaData
     {
         [Required]
@@ -20,7 +26,7 @@ namespace ParkingLotWebApp.Models
         [Required]
         [UIHint("UTCLocalTimeDisplay")]
         public System.DateTime CreateUTCTime { get; set; }
-    
+
         public virtual Cars Cars { get; set; }
         public virtual ICollection<CarsInParkingLotPath_Body> CarsInParkingLotPath_Body { get; set; }
         public virtual ParkingGrid ParkingGrid { get; set; }
