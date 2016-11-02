92483057b827bf56ec947c86b2eefc2eea88d637
ediux <ediuxcity@gmail.com>
2016-11-02 13:45:43 +0800

    [變更] 重新命名檔案 ManageViewModel.cs->HomeIndexViewModel.cs

---
+++ ParkingLotWebApp/Models/HomeIndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ParkingLotWebApp.Models
+{
+    public class HomeIndexViewModel
+    {
+        public Dictionary<string, IEnumerable<vw_ParkingLotGridRemain>> RemainSummary { get; set; }
+
+        private Dictionary<string, bool> objSelected;
+        public  Dictionary<string,bool> SelectedTowers { get { return objSelected; } }
+        public HomeIndexViewModel()
+        {
+            objSelected = new Dictionary<string, bool>();
+        }
+    }
+}
