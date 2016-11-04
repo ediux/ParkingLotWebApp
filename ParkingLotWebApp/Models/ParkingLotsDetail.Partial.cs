bf8d639ba74a02ffe360870f0955926975bc0057
ediux <ediux@msn.com>
2016-11-04 12:02:36 +0800

    [變更] 更新資料模型 加入Create靜態方法以建立新的執行個體預設內容值

--- ParkingLotWebApp/Models/ParkingLotsDetail.Partial.cs
+++ ParkingLotWebApp/Models/ParkingLotsDetail.Partial.cs
@@ -1,52 +1,81 @@
 namespace ParkingLotWebApp.Models
 {
+    using App_GlobalResources;
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-    
+    using My.Core.Infrastructures.Implementations.Resources;
+    using System.Collections.ObjectModel;
+
     /// <summary>
     /// 停車場資訊
     /// </summary>
     [MetadataType(typeof(ParkingLotsDetailMetaData))]
     public partial class ParkingLotsDetail
     {
+        public static ParkingLotsDetail Create()
+        {
+            return new ParkingLotsDetail()
+            {
+                Address = string.Empty,
+                Charge = string.Empty,
+                ID = 0,
+                LastUpdate = DateTime.Now,
+                Latitude = 0.0f,
+                Longitude = 0.0f,
+                Name = string.Empty,
+                ParkingLotsFloor = new Collection<ParkingLotsFloor>(),
+                Period = "",
+                Tel = "",
+                Void = false
+            };
+        }
     }
-    
+
     public partial class ParkingLotsDetailMetaData
     {
         [Required]
-        [Display(Name="停車場系統代碼")]
+        [Display(Name = "ParkingLotsDetail_ID", ResourceType = typeof(WebPages))]
         public int ID { get; set; }
-        
-        [StringLength(25, ErrorMessage="欄位長度不得大於 25 個字元")]
+
+        [StringLength(25, ErrorMessageResourceName = "StringLength", ErrorMessageResourceType = typeof(ErrorMessage))]
         [Required]
+        [Display(Name = "ParkingLotsDetail_Name", ResourceType = typeof(WebPages))]
         public string Name { get; set; }
-        
-        [StringLength(75, ErrorMessage="欄位長度不得大於 75 個字元")]
+
+        [StringLength(75, ErrorMessageResourceName = "StringLength", ErrorMessageResourceType = typeof(ErrorMessage))]
         [Required]
+        [Display(Name = "ParkingLotsDetail_Address", ResourceType = typeof(WebPages))]
         public string Address { get; set; }
-        
-        [StringLength(15, ErrorMessage="欄位長度不得大於 15 個字元")]
+
+        [StringLength(15, ErrorMessageResourceName = "StringLength", ErrorMessageResourceType = typeof(ErrorMessage))]
         [Required]
+        [Display(Name = "Tel", ResourceType = typeof(Common))]
+        [Phone]
         public string Tel { get; set; }
-        
-        [StringLength(25, ErrorMessage="欄位長度不得大於 25 個字元")]
+
+        [StringLength(25, ErrorMessageResourceName = "StringLength", ErrorMessageResourceType = typeof(ErrorMessage))]
         [Required]
+        [Display(Name = "ParkingLotsDetail_Period", ResourceType = typeof(WebPages))]
+        [RegularExpression("\\d{1,2}:\\d{1,2}~\\d{1,2}:\\d{1,2}",
+            ErrorMessageResourceName = "ParkingLotsDetail_Period_VaildErrorMessage",
+            ErrorMessageResourceType = typeof(WebPages))]
         public string Period { get; set; }
-        
-        [StringLength(100, ErrorMessage="欄位長度不得大於 100 個字元")]
+
+        [StringLength(100, ErrorMessageResourceName = "StringLength", ErrorMessageResourceType = typeof(ErrorMessage))]
         [Required]
+        [Display(Name = "Charge", ResourceType = typeof(Common))]
         public string Charge { get; set; }
         [Required]
+        [Display(Name = "Longitude", ResourceType = typeof(Common))]
         public double Longitude { get; set; }
         [Required]
+        [Display(Name = "Latitude", ResourceType = typeof(Common))]
         public double Latitude { get; set; }
         [Required]
-        public int CarGrid { get; set; }
-        [Required]
-        public int MotoGrid { get; set; }
-        [Required]
+        [Display(Name = "Void", ResourceType = typeof(MUI))]
         public bool Void { get; set; }
-        public Nullable<System.DateTime> LastUpdate { get; set; }
+        [Display(Name = "ParkingLotsDetail_LastUpdate", ResourceType = typeof(WebPages))]
+        public DateTime? LastUpdate { get; set; }
     }
 }
