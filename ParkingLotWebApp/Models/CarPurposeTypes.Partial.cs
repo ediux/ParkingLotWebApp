92fd91a38be928984fbce549ae223cadbd62bf6d
ediux <ediux@msn.com>
2016-11-06 02:00:53 +0800

    [變更] 加入車輛用途管理資料表

---
+++ ParkingLotWebApp/Models/CarPurposeTypes.Partial.cs
@@ -0,0 +1,27 @@
+namespace ParkingLotWebApp.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    
+    [MetadataType(typeof(CarPurposeTypesMetaData))]
+    public partial class CarPurposeTypes
+    {
+    }
+    
+    public partial class CarPurposeTypesMetaData
+    {
+        [Required]
+        public int Id { get; set; }
+        
+        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+        [Required]
+        [Display(Name="用途")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name="Void",ResourceType = typeof(My.Core.Infrastructures.Implementations.Resources.MUI))]
+        public bool Void { get; set; }
+    
+        public virtual ICollection<Cars> Cars { get; set; }
+    }
+}
