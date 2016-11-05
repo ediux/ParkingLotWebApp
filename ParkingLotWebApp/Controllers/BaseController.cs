2fde35ff1b513f41bfb1e678d523f85196f54d04
ediux <ediux@msn.com>
2016-11-06 02:09:11 +0800

    [加入] 加入共用的基底控制器類別

---
+++ ParkingLotWebApp/Controllers/BaseController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ParkingLotWebApp.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            base.OnException(filterContext);
+        }
+
+        protected override void HandleUnknownAction(string actionName)
+        {
+            base.HandleUnknownAction(actionName);
+        }
+    }
+}
