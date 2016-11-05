c9d928669e7e208d9949b9ca156a9bff8567d0d4
ediux <ediux@msn.com>
2016-11-06 02:36:10 +0800

    建立執行Create方法時候的初始化方法

--- ParkingLotWebApp/Models/PartialClasses/CarPurposeTypes.Partial.cs
+++ ParkingLotWebApp/Models/PartialClasses/CarPurposeTypes.Partial.cs
@@ -7,6 +7,14 @@ namespace ParkingLotWebApp.Models
 {
     public partial class CarPurposeTypes
     {
-        
+        public static CarPurposeTypes Create()
+        {
+            return new Models.CarPurposeTypes()
+            {
+                Id = 0,
+                Name = string.Empty,
+                Void = false
+            };
+        }
     }
 }
