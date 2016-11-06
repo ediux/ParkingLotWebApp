05d104b03c44abfcb3e9968af16f2043b9aaca0b
ediux <ediux@msn.com>
2016-11-06 09:16:56 +0800

    複寫車輛用途DAL的預設新增和刪除功能

--- ParkingLotWebApp/Models/PartialClasses/CarPurposeTypesRepository.Partial.cs
+++ ParkingLotWebApp/Models/PartialClasses/CarPurposeTypesRepository.Partial.cs
@@ -11,5 +11,27 @@ namespace ParkingLotWebApp.Models
         {
             return base.All().Where(w => w.Void == false);
         }
+
+        public override CarPurposeTypes Add(CarPurposeTypes entity)
+        {
+            CarPurposeTypes EntityInDatabase = ObjectSet.SingleOrDefault(w =>
+                w.Name.Equals(entity.Name, StringComparison.InvariantCultureIgnoreCase));
+
+            if (EntityInDatabase != null)
+            {
+                EntityInDatabase.Void = false;
+                UnitOfWork.Context.Entry(EntityInDatabase).State = System.Data.Entity.EntityState.Modified;
+                return EntityInDatabase;
+            }
+
+            return base.Add(entity);
+        }
+        public override void Delete(CarPurposeTypes entity)
+        {
+            if (entity.Void == false)
+            {
+                entity.Void = true;
+            }
+        }
     }
 }
