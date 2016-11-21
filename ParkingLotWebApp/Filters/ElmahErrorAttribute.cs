ccf133649e5a5bf81d9ea6da87c77da1d644317f
ediux <ediux@msn.com>
2016-11-21 18:50:37 +0800

    加入可攔截WebAPI產生的錯誤之動作過濾器

---
+++ ParkingLotWebApp/Filters/ElmahErrorAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Filters;
+
+namespace ParkingLotWebApp.Filters
+{
+    public class ElmahErrorAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        {
+            if (actionExecutedContext.Exception != null)
+                Elmah.ErrorSignal.FromCurrentContext().Raise(actionExecutedContext.Exception);
+
+            base.OnException(actionExecutedContext);
+        }
+    }
+}
