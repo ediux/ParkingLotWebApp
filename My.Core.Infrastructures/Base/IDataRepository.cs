65f470b48862fc8a12468c96d1072d62a9ee3216
ediux <ediux@msn.com>
2016-11-22 18:43:15 +0800

    加入記憶體快取機制EF的介面定義

---
+++ My.Core.Infrastructures/Base/IDataRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My.Core.Infrastructures.Implementations.Models
+{
+    public interface IDataRepository<T> where T:class
+    {
+        void ClearCache(string key);
+        IQueryable<T> GetCache();
+    }
+}
