ca9953feab68ae2018651d8bbd506654183f1549
ediux <ediux@msn.com>
2016-11-22 18:44:05 +0800

    加入實作記憶體快取的類別

---
+++ My.Core.Infrastructures.Implementations/Base/DefaultCacheProvider.cs
@@ -0,0 +1,33 @@
+﻿using My.Core.Infrastructures.Implementations.Models;
+using System;
+using System.Runtime.Caching;
+
+namespace My.Core.Infrastructures.Implementations.Base
+{
+    public class DefaultCacheProvider : ICacheProvider
+    {
+        //private  ObjectCache Cache { get { return MemoryCache.Default; } }
+
+        public object Get(string key)
+        {
+            return MemoryCache.Default[key];
+        }
+
+        public void Invalidate(string key)
+        {
+            MemoryCache.Default.Remove(key);
+        }
+
+        public bool IsSet(string key)
+        {
+            return (MemoryCache.Default[key] != null);
+        }
+
+        public void Set(string key, object data, int cacheTime)
+        {
+            CacheItemPolicy policy = new CacheItemPolicy();
+            policy.AbsoluteExpiration = DateTime.Now + TimeSpan.FromMinutes(cacheTime);
+            MemoryCache.Default.Add(new CacheItem(key, data), policy);
+        }
+    }
+}
