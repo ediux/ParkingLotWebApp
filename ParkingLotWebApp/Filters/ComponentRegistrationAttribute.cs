a345d2a9db87ef55d0f6cf7d3616c78adc75b89f
ediux <ediux@msn.com>
2016-11-22 18:51:18 +0800

    控制器元件註冊動作過濾器(權限相關)

---
+++ ParkingLotWebApp/Filters/ComponentRegistrationAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using My.Core.Infrastructures.Implementations.Models;
+
+namespace ParkingLotWebApp.Filters
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class ComponentRegistrationAttribute : ActionFilterAttribute, IActionFilter
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            ISystem_ControllersRepository CtrRepo = RepositoryHelper.GetSystem_ControllersRepository();
+            ISystem_ControllerActionsRepository ActionRepo = RepositoryHelper.GetSystem_ControllerActionsRepository(CtrRepo.UnitOfWork);
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
