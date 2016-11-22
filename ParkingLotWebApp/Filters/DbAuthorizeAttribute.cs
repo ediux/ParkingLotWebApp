3bf1ac108401918610ad2e442e433330cab910d5
ediux <ediux@msn.com>
2016-11-22 18:49:43 +0800

    加入自訂權限驗證

---
+++ ParkingLotWebApp/Filters/DbAuthorizeAttribute.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using My.Core.Infrastructures.Implementations.Models;
+
+namespace ParkingLotWebApp.Filters
+{
+    public class DbAuthorizeAttribute : AuthorizeAttribute
+    {
+        private IApplicationRoleRepository roleRepo;
+
+        public DbAuthorizeAttribute()
+        {
+            roleRepo = RepositoryHelper.GetApplicationRoleRepository();
+
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            base.HandleUnauthorizedRequest(filterContext);
+        }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (httpContext == null)
+                throw new ArgumentNullException("httpContext");
+
+            string[] _allowroles = this.Roles.Split(',');
+
+            List<ApplicationRole> rolesInDb = roleRepo.Where(w => _allowroles.Contains(w.Name)).ToList();
+
+            return base.AuthorizeCore(httpContext);
+        }
+
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            base.OnAuthorization(filterContext);
+        }
+
+        protected override HttpValidationStatus OnCacheAuthorization(HttpContextBase httpContext)
+        {
+            return base.OnCacheAuthorization(httpContext);
+        }
+    }
+}
