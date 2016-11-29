5b49fa8df94892022fd114156bca0e25764a8248
ediux <ediux@msn.com>
2016-11-29 09:35:34 +0800

    以資料庫為主的權限驗證控器過濾器

--- ParkingLotWebApp/Filters/DbAuthorizeAttribute.cs
+++ ParkingLotWebApp/Filters/DbAuthorizeAttribute.cs
@@ -16,17 +16,46 @@ namespace ParkingLotWebApp.Filters
             roleRepo = RepositoryHelper.GetApplicationRoleRepository();
 
         }
-
-        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        public override void OnAuthorization(AuthorizationContext filterContext)
         {
-            if (httpContext == null)
-                throw new ArgumentNullException("httpContext");
+            string actionName = filterContext.ActionDescriptor.ActionName;
+            string controllerName = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName;
+            string controllerClassName = filterContext.ActionDescriptor.ControllerDescriptor.ControllerType.Name;
+            string controllerNamespace = filterContext.ActionDescriptor.ControllerDescriptor.ControllerType.Namespace;
+            ApplicationUser currentUser = null;
 
-            string[] _allowroles = this.Roles.Split(',');
+            IApplicationUserRepository userRepo = RepositoryHelper.GetApplicationUserRepository();
 
-            List<ApplicationRole> rolesInDb = roleRepo.Where(w => _allowroles.Contains(w.Name)).ToList();
+            ISystem_ControllerActionsRepository actionRepo = RepositoryHelper.GetSystem_ControllerActionsRepository(userRepo.UnitOfWork);
 
-            return base.AuthorizeCore(httpContext);
+            currentUser = userRepo.FindUserByLoginAccount(filterContext.HttpContext.User.Identity.Name, false);
+
+            if (currentUser != null)
+            {
+                if (currentUser.ApplicationRole.SelectMany(s => s.System_ControllerActions)
+                        .Where(w => w.Name == actionName)
+                        .Where(w => w.System_Controllers.Name == controllerName
+                        && w.System_Controllers.ClassName == controllerClassName
+                        && w.System_Controllers.Namespace == controllerNamespace)
+                        .Any())
+                {
+                    return;
+                }
+
+            }
+
+            if (filterContext.ActionDescriptor.GetCustomAttributes(true).OfType<AllowAnonymousAttribute>().Any())
+                return;
+
+            if (filterContext.ActionDescriptor.ControllerDescriptor.GetCustomAttributes(true).OfType<AllowAnonymousAttribute>().Any())
+                return;
+
+            if(filterContext.HttpContext.User.Identity.Name.Equals("root", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return;
+            }
+
+            filterContext.Result = new HttpUnauthorizedResult();
         }
 
     }
